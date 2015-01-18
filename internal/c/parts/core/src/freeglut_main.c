dcaf43a4936c2d2451c3c4c1119390777518042c
Galleon <galleondragon@gmail.com>
2015-01-18 05:55:56 -0800

    Fix for Linux (running setup fails on prev dirty build)

--- internal/c/parts/core/src/freeglut_main.c
+++ internal/c/parts/core/src/freeglut_main.c
@@ -44,11 +44,10 @@
 #endif
 
 int qb64_custom_event(int event,int v1,int v2,int v3,int v4,int v5,int v6,int v7,int v8,void *p1,void *p2);
-#ifdef TARGET_HOST_MS_WINDOWS
- LRESULT qb64_os_event_windows(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam, int *qb64_os_event_info);
-#endif
 #if TARGET_HOST_POSIX_X11
  void qb64_os_event_linux(XEvent *event, int *qb64_os_event_info);
+#else
+ LRESULT qb64_os_event_windows(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam, int *qb64_os_event_info);
 #endif
 
 #define QB64_EVENT_CLOSE 1
