a25cb9a846bb28027e373d5635bcb1089f490394
Luke Ceddia <flukiluke@gmail.com>
2015-08-09 21:33:22 +1000

    Temporaraily commented out Linux version of _SCREENICON function due to compile errors

--- internal/c/parts/user_mods/src/Steve Stuff/screeninfo.cpp
+++ internal/c/parts/user_mods/src/Steve Stuff/screeninfo.cpp
@@ -23,24 +23,27 @@ int32 func__controlchr () {
 }
 
 int32 func_screenicon () {
-	while (!window_exists){Sleep(100);}
-	extern int32 screen_hide;
-	if (screen_hide) {error(5); return 0;}
-	#ifdef QB64_WINDOWS
-        #include <windows.h>
-		extern HWND window_handle;
-	    return -IsIconic(window_handle);
-    #else
-     	#include <X11/X.h>
-		#include <X11/Xlib.h>
-		extern Display *X11_display;
-		extern Window X11_window;
-		extern int32 screen_hide;
-		XWindowAttributes attribs;
-		while (!(X11_display && X11_window));
-		XGetWindowAttributes(X11_display, X11_window, &attribs);
-		if (attribs.map_state == IsUnmapped) return -1;
-		return 0;
-    #endif
-	return 0; //if we get here and haven't exited already, we failed somewhere along the way.
+  while (!window_exists){Sleep(100);}
+  extern int32 screen_hide;
+  if (screen_hide) {error(5); return 0;}
+#ifdef QB64_WINDOWS
+#include <windows.h>
+  extern HWND window_handle;
+  return -IsIconic(window_handle);
+#else
+  /*     
+	 Linux code not compiling for now
+	 #include <X11/X.h>
+	 #include <X11/Xlib.h>
+	 extern Display *X11_display;
+	 extern Window X11_window;
+	 extern int32 screen_hide;
+	 XWindowAttributes attribs;
+	 while (!(X11_display && X11_window));
+	 XGetWindowAttributes(X11_display, X11_window, &attribs);
+	 if (attribs.map_state == IsUnmapped) return -1;
+	 return 0;
+	 #endif */
+  return 0; //if we get here and haven't exited already, we failed somewhere along the way.
+  #endif
 }
