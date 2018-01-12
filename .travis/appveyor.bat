65462c76ee5f5c35466ef6d2ee0b599e556ce5c2
Luke Ceddia <flukiluke@gmail.com>
2018-01-13 01:02:20 +1100

    Correct order of cleanup

--- .travis/appveyor.bat
+++ .travis/appveyor.bat
@@ -40,12 +40,13 @@ c_compiler\bin\windres.exe -i ..\temp\icon.rc -o ..\temp\icon.o
 c_compiler\bin\g++ -mconsole -s -Wfatal-errors -w -Wall qbx.cpp libqb\os\win\libqb_setup.o ..\temp\icon.o -D DEPENDENCY_LOADFONT  parts\video\font\ttf\os\win\src.o -D DEPENDENCY_SOCKETS -D DEPENDENCY_NO_PRINTER -D DEPENDENCY_ICON -D DEPENDENCY_NO_SCREENIMAGE parts\core\os\win\src.a -lopengl32 -lglu32   -mwindows -static-libgcc -static-libstdc++ -D GLEW_STATIC -D FREEGLUT_STATIC     -lws2_32 -lwinmm -lgdi32 -o "..\..\qb64_bootstrap.exe"
 IF ERRORLEVEL 1 exit /b 1
 cd ..\..
-del qb64_bootstrap.exe
 
 echo AutoBuildMsg$ = CHR$(10) + "From git %APPVEYOR_REPO_COMMIT:~0,7%" + CHR$(10) >> source\global\version.bas
 qb64_bootstrap.exe -x source\qb64.bas -o qb64.exe
 IF ERRORLEVEL 1 exit /b 1
 
+del qb64_bootstrap.exe
+del secure-file
 del /q /s internal\source\*
 move internal\temp\* internal\source\
 del /q /s internal\c\libqb\*.o >nul 2>nul
