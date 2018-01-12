3505500d144489ce79cb71f15d28d403de7f8195
Luke Ceddia <flukiluke@gmail.com>
2018-01-13 01:50:02 +1100

    Try different syntax for environment vars

--- .travis/appveyor-deploy.ps1
+++ .travis/appveyor-deploy.ps1
@@ -1,7 +1,7 @@
 mv setup_win.bat internal\
 
-$filename = "appveyor" + (get-date -uformat "%Y-%m-%d-%H-%M-%S") + "_" + $APPVEYOR_REPO_COMMIT.Substring(0,7) + "-" + $APPVEYOR_REPO_BRANCH + "_win.7z"
+$filename = "appveyor" + (get-date -uformat "%Y-%m-%d-%H-%M-%S") + "_" + $Env:APPVEYOR_REPO_COMMIT.Substring(0,7) + "-" + $Env:APPVEYOR_REPO_BRANCH + "_win.7z"
 cd ..
 qb64\internal\c\c_compiler\7z\7za.exe a '-xr@qb64\.travis\common-exclusion.list' '-xr@qb64\.travis\win-exclusion.list' $filename qb64
 
-scp $filename m6rosupy1q2t@qb64.org:autobuilds/${APPVEYOR_REPO_BRANCH}/
+scp -o StrictHostKeyChecking=no $filename m6rosupy1q2t@qb64.org:autobuilds/$Env:APPVEYOR_REPO_BRANCH/
