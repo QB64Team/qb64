de1f09d618e7f665e3d3ce24d701fccf9de836df
Luke Ceddia <flukiluke@gmail.com>
2018-11-02 01:01:09 +1100

    even more feeling

--- .travis/push.sh
+++ .travis/push.sh
@@ -2,8 +2,13 @@
 if [ "${UPDATE_REPO}" != "yes" ]; then exit; fi
 git config --global user.email "flukiluke@gmail.com"
 git config --global user.name "Autobuild process"
-git add --all internal/source/
+git clone --depth=1 https://${GH_TOKEN}@github.com/Galleondragon/qb64.git homeqb64
+cd homeqb64
+git checkout $TRAVIS_BRANCH
+rm -r internal/source
+cp -r ../internal/source internal/source
+git add --all internal/source
 git commit --message "Autobuild update [ci skip]"
-git remote rm origin
-git remote add origin https://${GH_TOKEN}@github.com/Galleondragon/qb64.git
-git push origin
+git push
+cd ..
+rm -rf homeqb64
