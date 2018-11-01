99bb3ca5bccd1dbdccf623145b0c55d9c384fc0a
Luke Ceddia <flukiluke@gmail.com>
2018-11-01 23:48:30 +1100

    Attempt enabling internal/source/ updater

--- .travis/push.sh
+++ .travis/push.sh
@@ -5,8 +5,8 @@ git config --global user.name "Autobuild process"
 git add --all internal/source/
 git commit --message "Autobuild update [ci skip]"
 git remote rm origin
-git remote add origin https://${GH_TOKEN}@github.com/flukiluke/qb64.git > /dev/null 2>&1
-git push origin HEAD:master > /dev/null 2>&1
+git remote add origin https://${GH_TOKEN}@github.com/Galleondragon/qb64.git
+git push origin HEAD:master
 
 
 
