4990cb798de5f4b7a83770d8845590a35a23c864
Luke Ceddia <git@alephc.xyz>
2020-01-04 22:18:31 +1100

    Push code update back to repo

--- .ci/update-source.sh
+++ .ci/update-source.sh
@@ -6,3 +6,10 @@ find . -type f -iname "*.a" -exec rm {} \;
 find . -type f -iname "*.o" -exec rm {} \;
 cd internal/source
 rm debug_* recompile_*
+cd ../..
+
+git config --local user.email "flukiluke@gmail.com"
+git config --local user.name "Autobuild Process"
+git add internal/source
+git commit -m "Update internal/source"
+
