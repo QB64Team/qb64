39f22a41b515f4aa691e4d75ad54e8a53e0622c0
Luke Ceddia <git@alephc.xyz>
2020-01-04 23:09:53 +1100

    Proper compile exit code

--- .ci/compile.sh
+++ .ci/compile.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 echo From git `echo $GITHUB_SHA | sed 's/\(.......\).*$/\1/'` > internal/version.txt
-./qb64_bootstrap -x source/qb64.bas > /tmp/qb64-output
+./qb64_bootstrap -x -v source/qb64.bas
+SUCCESS=$?
 rm qb64_bootstrap
-if [ `grep -v '^WARNING' /tmp/qb64-output | wc -l` -gt 2 ]; then
-  cat /tmp/qb64-output
-  rm /tmp/qb64-output
-  exit 1
-fi
+exit $SUCCESS
