a3e4a2c2bb0f995a9293202136b98dc8e3a41de9
Luke Ceddia <git@alephc.xyz>
2020-01-04 23:40:28 +1100

    Only update source on change

--- .ci/update-source.sh
+++ .ci/update-source.sh
@@ -11,5 +11,7 @@ cd ../..
 git config --local user.email "flukiluke@gmail.com"
 git config --local user.name "Autobuild Process"
 git add internal/source
-git commit -m "Update internal/source"
+if [[ $(git diff --cached | wc -l) -gt 0 ]]
+    then git commit -m "Update internal/source"
+fi
 
