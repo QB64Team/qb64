b6aba842c46f5df70b0a3ad1fc825ca3ad92256e
Luke Ceddia <git@alephc.xyz>
2020-01-05 00:40:05 +1100

    Add ssh deployment

---
+++ .ci/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+TZ=UTC now=`date +"%F-%H-%M-%S"`
+filename="/tmp/qb64_${now}_`echo ${GITHUB_SHA} | sed 's/\(.......\).*$/\1/'`_lnx.tar.gz"
+cd ..
+tar --create --auto-compress --file ${filename} --exclude-from=qb64/.ci/common-exclusion.list --exclude-from=qb64/.ci/lnx-exclusion.list qb64
+
+# Send to server
+# Sometimes the connection can be a bit flakey, so try multiple times on error
+for i in `seq 1 10`
+do scp ${filename} remote-server:autobuilds/development/
+    if [ "$?" -eq 0 ]
+    then exit 0
+    fi
+    echo scp $i failed
+done
+
