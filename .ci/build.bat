d961d4dbed2dd50bac8f56863f9a90aebc49e860
FellippeHeitor <fellippeheitor@gmail.com>
2020-01-08 12:01:46 -0300

    Attempts to deliver qb64.exe with the proper icon. Also:
    - Deletes /.travis

--- .ci/build.bat
+++ .ci/build.bat
@@ -2,9 +2,12 @@ SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
 
 echo Compiling new QB64
 echo From git %APPVEYOR_REPO_COMMIT:~0,7% > internal\version.txt
-qb64_bootstrap.exe -x source\qb64.bas -o qb64.exe
+qb64_bootstrap.exe -z source\qb64.bas
+cd internal\c
+c_compiler\bin\g++ -mconsole -s -Wfatal-errors -w -Wall qbx.cpp libqb\os\win\libqb_setup.o ..\temp\icon.o -D DEPENDENCY_LOADFONT  parts\video\font\ttf\os\win\src.o -D DEPENDENCY_SOCKETS -D DEPENDENCY_NO_PRINTER -D DEPENDENCY_ICON -D DEPENDENCY_NO_SCREENIMAGE parts\core\os\win\src.a -lopengl32 -lglu32   -mwindows -static-libgcc -static-libstdc++ -D GLEW_STATIC -D FREEGLUT_STATIC     -lws2_32 -lwinmm -lgdi32 -o "..\..\qb64.exe"
 IF ERRORLEVEL 1 exit /b 1
 
+cd ..\..
 del qb64_bootstrap.exe
 del /q /s secure-file
 del /q /s internal\source\*
