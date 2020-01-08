ff4e78c759f6b56f9ff293831326033e4215fbd5
flukiluke <flukiluke@gmail.com>
2020-01-08 22:11:07 +1100

    Remove qb64_bootstrap.exe after use

--- .ci/deploy.bat
+++ .ci/deploy.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
-
+del qb64_bootstrap.exe
 cd ..
 set TIMEX=%TIME: =0%
 set archive=qb64_%DATE:~10,4%-%DATE:~7,2%-%DATE:~4,2%-%TIMEX:~0,2%-%TIMEX:~3,2%-%TIMEX:~6,2%_%GITHUB_SHA:~0,7%_win-%PLATFORM%.7z
