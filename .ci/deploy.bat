2753a3f9afd2e53601ba45fa2dfacf2477d01977
Luke Ceddia <git@alephc.xyz>
2020-01-08 20:47:53 +1100

    Fix typos?

--- .ci/deploy.bat
+++ .ci/deploy.bat
@@ -1,8 +1,7 @@
-@ECHO OFF
 SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
 
 cd ..
-set filename=qb64_%DATE:~10,4%-%DATE:~7,2%-%DATE:~4,2%-%TIME:~0,2%-%TIME:~3,2%-%TIME:~6,2%_%GITHUB_SHA:~0,7%_win-%PLATFORM%.7z
-7z a '-xr@qb64\.ci\common-exclusion.list' '-xr@qb64\.travis\win-exclusion.list' %filename% qb64
-scp %filename% remote-server:autobuilds/development/
+set archive=qb64_%DATE:~10,4%-%DATE:~7,2%-%DATE:~4,2%-%TIME:~0,2%-%TIME:~3,2%-%TIME:~6,2%_%GITHUB_SHA:~0,7%_win-%PLATFORM%.7z
+7z a -xr@qb64\.ci\common-exclusion.list -xr@qb64\.travis\win-exclusion.list %filename% qb64
+scp %archive% remote-server:autobuilds/development/
 
