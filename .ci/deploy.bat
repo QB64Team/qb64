63e1dabf2f4f2cb4b68bac3aec069481dd89b8ad
Luke Ceddia <git@alephc.xyz>
2020-01-08 21:24:26 +1100

    Work in the morning

--- .ci/deploy.bat
+++ .ci/deploy.bat
@@ -1,7 +1,8 @@
 SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
 
 cd ..
-set archive=qb64_%DATE:~10,4%-%DATE:~7,2%-%DATE:~4,2%-%TIME:~0,2%-%TIME:~3,2%-%TIME:~6,2%_%GITHUB_SHA:~0,7%_win-%PLATFORM%.7z
+set TIMEX=%TIME: =0%
+set archive=qb64_%DATE:~10,4%-%DATE:~7,2%-%DATE:~4,2%-%TIMEX:~0,2%-%TIMEX:~3,2%-%TIMEX:~6,2%_%GITHUB_SHA:~0,7%_win-%PLATFORM%.7z
 7z a -xr@qb64\.ci\common-exclusion.list -xr@qb64\.travis\win-exclusion.list %filename% qb64
 scp %archive% remote-server:autobuilds/development/
 
