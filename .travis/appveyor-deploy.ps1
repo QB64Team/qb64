f95a0bd3ce2e3cf355ab6dbd50965ffb50289e9c
FellippeHeitor <fellippeheitor@gmail.com>
2020-01-08 12:47:17 -0300

    Properly removes .travis. Replaces .ci/win-exclusion.list.

--- .travis/appveyor-deploy.ps1
+++
@@ -1,5 +0,0 @@
-$filename = "qb64_" + (get-date -uformat "%Y-%m-%d-%H-%M-%S") + "_" + $Env:APPVEYOR_REPO_COMMIT.Substring(0,7) + "-" + $Env:APPVEYOR_REPO_BRANCH + "_win_" + $Env:PLATFORM + ".7z"
-cd ..
-7z a '-xr@qb64\.travis\common-exclusion.list' '-xr@qb64\.travis\win-exclusion.list' $filename qb64
-
-scp -q -o StrictHostKeyChecking=no $filename m6rosupy1q2t@qb64.org:autobuilds/$Env:APPVEYOR_REPO_BRANCH/
