c52211ffc5c2382db475608e68c10a3e59c373d9
Luke Ceddia <git@alephc.xyz>
2020-01-08 21:31:35 +1100

    More typos

--- .ci/deploy.bat
+++ .ci/deploy.bat
@@ -3,6 +3,6 @@ SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
 cd ..
 set TIMEX=%TIME: =0%
 set archive=qb64_%DATE:~10,4%-%DATE:~7,2%-%DATE:~4,2%-%TIMEX:~0,2%-%TIMEX:~3,2%-%TIMEX:~6,2%_%GITHUB_SHA:~0,7%_win-%PLATFORM%.7z
-7z a -xr@qb64\.ci\common-exclusion.list -xr@qb64\.travis\win-exclusion.list %filename% qb64
+7z a -xr@qb64\.ci\common-exclusion.list -xr@qb64\.travis\win-exclusion.list %archive% qb64
 scp %archive% remote-server:autobuilds/development/
 
