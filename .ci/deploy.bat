a3f238b8ef83f02f5381d2a068a4c85b2ea9cc05
Fellippe Heitor <fellippeheitor@gmail.com>
2020-01-29 12:45:38 -0300

    Fixes output of versioninfo.txt

--- .ci/deploy.bat
+++ .ci/deploy.bat
@@ -17,7 +17,7 @@ FOR /L %%i IN (1,1,10) DO (
 :doversion
 
 set archive=versioninfo.txt
-echo Dev build generated on %DATE:~10,4%-%DATE:~4,2%-%DATE:~7,2%, %TIMEX:~0,2%-%TIMEX:~3,2%-%TIMEX:~6,2%, from git ^<a href=https://github.com/QB64Team/qb64/commits/development^>%GITHUB_SHA:~0,7%^</a^> > %archive%
+echo Dev build generated on %DATE:~10,4%-%DATE:~4,2%-%DATE:~7,2%, from git ^<a href="https://github.com/QB64Team/qb64/commits/development"^>%GITHUB_SHA:~0,7%^</a^> > %archive%
 
 FOR /L %%i IN (1,1,10) DO (
     scp %archive% remote-server:autobuilds/development/
