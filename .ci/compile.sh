9e0a354d9139e206fea68c0305803f58dcc40784
Luke Ceddia <git@alephc.xyz>
2020-07-13 20:51:11 +1000

    Package internal/source for macOS, exclude for windows

--- .ci/compile.sh
+++ .ci/compile.sh
@@ -4,4 +4,9 @@ echo From git `echo $GITHUB_SHA | sed 's/\(.......\).*$/\1/'` > internal/version
 ./qb64_bootstrap -x -v source/qb64.bas
 SUCCESS=$?
 rm qb64_bootstrap
+rm internal/source/*
+mv internal/temp/* internal/source/
+rm internal/source/debug_* internal/source/recompile_*
+find . -type f -iname "*.a" -exec rm {} \;
+find . -type f -iname "*.o" -exec rm {} \;
 exit $SUCCESS
--- .ci/update-source.sh
+++ .ci/update-source.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-rm internal/source/*
-mv internal/temp/* internal/source/
-find . -type f -iname "*.a" -exec rm {} \;
-find . -type f -iname "*.o" -exec rm {} \;
-cd internal/source
-rm debug_* recompile_*
-cd ../..
-
 git config --local user.email "flukiluke@gmail.com"
 git config --local user.name "Autobuild Process"
 git add internal/source
