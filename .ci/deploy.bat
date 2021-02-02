36a0a28e5926b64b78b3a1e61159ab2bbf49737e
Luke Ceddia <flukiluke@gmail.com>
2021-02-02 21:36:46 +1100

    Remove quotes?

--- .ci/deploy.bat
+++ .ci/deploy.bat
@@ -6,7 +6,7 @@ set TIMEX=%TIME: =0%
 set archive=qb64_%DATE:~10,4%-%DATE:~4,2%-%DATE:~7,2%-%TIMEX:~0,2%-%TIMEX:~3,2%-%TIMEX:~6,2%_%GITHUB_SHA:~0,7%_win-%PLATFORM%.7z
 7z a -xr@qb64\.ci\common-exclusion.list -xr@qb64\.ci\win-exclusion.list %archive% qb64
 
-aws --output text --query 'Contents[].Key' s3api list-objects --bucket %BUCKET% --prefix win-%PLATFORM% > olddevbuilds.txt
+aws --output text --query Contents[].Key s3api list-objects --bucket %BUCKET% --prefix win-%PLATFORM% > olddevbuilds.txt
 
 aws s3 cp %archive% s3://%BUCKET%/win-%PLATFORM%/
 
