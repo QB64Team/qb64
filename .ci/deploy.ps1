5ef682c7ca2237d40820050aa815a4ef72782710
Luke Ceddia <luke@alephc.xyz>
2021-02-02 22:52:33 +1100

    Quote 7z command

--- .ci/deploy.ps1
+++ .ci/deploy.ps1
@@ -3,7 +3,7 @@ $Timestamp = (Get-Date).ToUniversalTime().ToString("yyyy-MM-dd-HH-mm-ss")
 $Filename = "qb64_${Timestamp}_$($Env:GITHUB_SHA.substring(0,7))_win-${Env:PLATFORM}.7z"
 
 Set-Location ..
-7z a -xr@qb64\.ci\common-exclusion.list -xr@qb64\.ci\win-exclusion.list $Filename qb64
+7z a '-xr@qb64\.ci\common-exclusion.list' '-xr@qb64\.ci\win-exclusion.list' $Filename qb64
 $OldFiles = aws --output json --query Contents[].Key s3api list-objects --bucket $Bucket --prefix win-$Env:PLATFORM | ConvertFrom-Json
 aws s3 cp $Filename "s3://${Bucket}/win-${Env:PLATFORM}/"
 foreach ($f in $OldFiles) {
