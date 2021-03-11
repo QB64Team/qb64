fee91e8c33b9e3de36bdd55cb62b2f347b7b11b8
Doug Kearns <dougkearns@gmail.com>
2021-03-11 11:27:56 +1100

    Fixes missing file messages when purging precompiled content

--- internal/c/purge_all_precompiled_content_lnx.sh
+++ internal/c/purge_all_precompiled_content_lnx.sh
@@ -1,32 +1,32 @@
 #!/bin/sh
 
-rm libqb/os/win/*.o
-rm libqb/os/lnx/*.o
+rm -f libqb/os/win/*.o
+rm -f libqb/os/lnx/*.o
 
-rm parts/core/os/win/src.a
-rm parts/core/os/lnx/src.a
-rm parts/core/os/win/temp/*.o
-rm parts/core/os/lnx/temp/*.o
+rm -f parts/core/os/win/src.a
+rm -f parts/core/os/lnx/src.a
+rm -f parts/core/os/win/temp/*.o
+rm -f parts/core/os/lnx/temp/*.o
 
-rm parts/audio/decode/mp3/os/win/src.a
-rm parts/audio/decode/mp3/os/lnx/src.a
-rm parts/audio/decode/mp3/os/win/temp/*.o
-rm parts/audio/decode/mp3/os/lnx/temp/*.o
+rm -f parts/audio/decode/mp3/os/win/src.a
+rm -f parts/audio/decode/mp3/os/lnx/src.a
+rm -f parts/audio/decode/mp3/os/win/temp/*.o
+rm -f parts/audio/decode/mp3/os/lnx/temp/*.o
 
-rm parts/audio/decode/ogg/os/win/src.o
-rm parts/audio/decode/ogg/os/lnx/src.o
-rm parts/audio/decode/ogg/os/win/temp/*.o
-rm parts/audio/decode/ogg/os/lnx/temp/*.o
+rm -f parts/audio/decode/ogg/os/win/src.o
+rm -f parts/audio/decode/ogg/os/lnx/src.o
+rm -f parts/audio/decode/ogg/os/win/temp/*.o
+rm -f parts/audio/decode/ogg/os/lnx/temp/*.o
 
-rm parts/audio/conversion/os/win/src.a
-rm parts/audio/conversion/os/lnx/src.a
-rm parts/audio/conversion/os/win/temp/*.o
-rm parts/audio/conversion/os/lnx/temp/*.o
+rm -f parts/audio/conversion/os/win/src.a
+rm -f parts/audio/conversion/os/lnx/src.a
+rm -f parts/audio/conversion/os/win/temp/*.o
+rm -f parts/audio/conversion/os/lnx/temp/*.o
 
-rm parts/audio/out/os/win/src.a
-rm parts/audio/out/os/lnx/src.a
-rm parts/audio/out/os/win/temp/*.o
-rm parts/audio/out/os/lnx/temp/*.o
+rm -f parts/audio/out/os/win/src.a
+rm -f parts/audio/out/os/lnx/src.a
+rm -f parts/audio/out/os/win/temp/*.o
+rm -f parts/audio/out/os/lnx/temp/*.o
 
-rm parts/video/font/ttf/os/win/src.o
-rm parts/video/font/ttf/os/lnx/src.o
+rm -f parts/video/font/ttf/os/win/src.o
+rm -f parts/video/font/ttf/os/lnx/src.o
