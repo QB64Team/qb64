cc6cc2a47466b0191340e79d7d6482e8fb5a2251
all-other-usernames-were-taken <74026992+all-other-usernames-were-taken@users.noreply.github.com>
2021-07-22 21:17:13 -0400

    comment

--- internal/support/auto-term.sh
+++ internal/support/auto-term.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# list taken from i3-sensible-terminal(1)
 terms="'$TERM' '$TERMINAL' terminal x-terminal-emulator xdg-terminal urxvt rxvt termit terminator Eterm aterm uxterm xterm gnome-terminal roxterm xfce4-terminal termite lxterminal mate-terminal terminology st qterminal lilyterm tilix terminix konsole kitty guake tilda alacritty hyper"
 
 for term in $terms; do
