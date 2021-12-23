df07ffa91a7845ce239f58a5168b487d7e1e3032
Luke Ceddia <flukiluke@gmail.com>
2021-12-23 22:38:03 +1100

    Change to DigitalOcean Spaces for dev builds

--- .ci/deploy.ps1
+++ .ci/deploy.ps1
@@ -1,13 +1,8 @@
-$Bucket = "qb64-development-builds"
+$Bucket = "qb64"
 $Timestamp = (Get-Date).ToUniversalTime().ToString("yyyy-MM-dd-HH-mm-ss")
-$Filename = "qb64_${Timestamp}_$($Env:GITHUB_SHA.substring(0,7))_win-${Env:PLATFORM}.7z"
-
+$Filename = "qb64_development_win-${Env:PLATFORM}.7z"
+$Dirname = "qb64_${Timestamp}_$($Env:GITHUB_SHA.substring(0,7))_win-${Env:PLATFORM}"
 Set-Location ..
-7z a '-xr@qb64\.ci\common-exclusion.list' '-xr@qb64\.ci\win-exclusion.list' $Filename qb64
-$OldFiles = aws ${Env:S3_ENDPOINT} --output json --query Contents[].Key s3api list-objects --bucket $Bucket --prefix win-${Env:PLATFORM} | ConvertFrom-Json
-aws s3 cp $Filename "s3://${Bucket}/win-${Env:PLATFORM}/"
-Set-Content -Path latest.txt -NoNewline -Value $Filename
-foreach ($f in $OldFiles) {
-    aws s3 rm "s3://$Bucket/$f"
-}
-aws s3 cp latest.txt "s3://${Bucket}/win-${Env:PLATFORM}/"
+Rename-Item qb64 $Dirname
+7z a "-xr@${Dirname}\.ci\common-exclusion.list" "-xr@${Dirname}\.ci\win-exclusion.list" $Filename $Dirname 
+aws --endpoint-url ${S3_ENDPOINT} s3 cp $Filename "s3://${Bucket}/development-builds/${Filename}"
